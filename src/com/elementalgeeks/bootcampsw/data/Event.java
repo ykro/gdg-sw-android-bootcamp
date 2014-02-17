cc941e28c0a757473f9450ca0670aac98212a682
ykro <aacs85@gmail.com>
2014-02-16 18:26:29 -0600

    changes to remove unused code

--- src/com/elementalgeeks/bootcampsw/data/Event.java
+++ src/com/elementalgeeks/bootcampsw/data/Event.java
@@ -46,46 +46,6 @@ public class Event {
 	public void setId(String id) {
 		this.id = id;
 	}
-	/*
-
-	private static final String KEY_ID = "id";
-	private static final String KEY_LAT = "lat";
-	private static final String KEY_LNG = "lng";	
-	private static final String KEY_DATE = "date";
-	private static final String KEY_CITY = "city";	
-	private static final String KEY_COUNTRY = "country";
-
-	public JSONObject toJSONObject() {
-		JSONObject obj = new JSONObject();
-        try {
-            obj.put(KEY_ID, id);
-            obj.put(KEY_CITY, city);
-            obj.put(KEY_COUNTRY, country);
-            obj.put(KEY_LAT, lat);
-            obj.put(KEY_LNG, lng);
-            
-            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-            obj.put(KEY_DATE, dateFormat.format(date));            
-        } catch (JSONException e) {}
-		return obj;
-	}
-	
-	public void fromJSONString(String strjson) {		
-		try {
-			JSONObject obj = new JSONObject(strjson);
-			this.id = obj.getString(KEY_ID);
-			this.city = obj.getString(KEY_CITY);
-			this.country = obj.getString(KEY_COUNTRY);
-			this.lat = obj.getDouble(KEY_LAT);
-			this.lng = obj.getDouble(KEY_LNG);
-			
-			SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");  			
-			try {  
-				this.date = dateFormat.parse(obj.getString(KEY_DATE));  
-			} catch (ParseException pe) {}			            						
-		} catch (JSONException e) {}		
-	}
-	*/
 	public String getWebsite() {
 		return website;
 	}
